d1d9ae393248a3f800cd2e034fe2e998fe36896a
Oliver Trosien <oliver.trosien@zalando.de>
2017-03-09 09:59:29 +0100

    Upgrade default swagger-codegen to 2.2.2

--- src/main/java/io/swagger/codegen/gradle/SwaggerToolingExtension.java
+++ src/main/java/io/swagger/codegen/gradle/SwaggerToolingExtension.java
@@ -2,7 +2,7 @@ package io.swagger.codegen.gradle;
 
 public class SwaggerToolingExtension {
 
-    private String codegenVersion = "2.2.1";
+    private String codegenVersion = "2.2.2";
 
     public void setCodegenVersion(String codegenVersion) {
         this.codegenVersion = codegenVersion;

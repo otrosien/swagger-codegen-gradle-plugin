a56928f7143c086a32b6b504861a800fa537cd95
Oliver Trosien <oliver.trosien@zalando.de>
2017-03-09 12:48:12 +0100

    don't delete output dir (would cause extra friction in IDEs)

--- src/main/java/io/swagger/codegen/gradle/SwaggerCodegenTask.java
+++ src/main/java/io/swagger/codegen/gradle/SwaggerCodegenTask.java
@@ -404,10 +404,6 @@ public class SwaggerCodegenTask extends DefaultTask {
     @TaskAction
     public void invokeSwaggerCodegen() throws Exception {
         validateOutputDir();
-        if(!isSkipOverwrite()) {
-            getProject().delete(getOutputDir());
-        }
-
         Set<File> files = getProject().getConfigurations().getByName("swaggerCodegen").getFiles();
         Set<URL> urls = new HashSet<>(files.size());
         for (File file : files) {
